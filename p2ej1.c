5b1ca3341b44ac187637c526f2f2bae70017d712
shadexlol <shadexn1@gmail.com>
2016-10-12 20:05:01 +0200

    Add files via upload

---
+++ p2ej1.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#define PI 3.141592654  // PI es una constante que podemos usar
+
+int main()
+{
+	float a;
+	float res;
+	
+	printf("un valor para el seno de A, comprendido entre 0 y 2pi \n");
+	scanf("%f",&a);
+	
+	printf ("el valor del seno es: %f \n", sin(a));
+	printf ("entonces el valor del arsen es; %f \n", asin(sin(a)));break;
+	
+	return 0;
+	
+}
