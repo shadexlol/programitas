2d4f19257e7b58de7a1d04bea6ead39ffa5b951c
shadexlol <shadexn1@gmail.com>
2016-10-18 16:56:08 +0200

    Add files via upload

---
+++ ACADEMIA/milibreria.h
@@ -0,0 +1,14 @@
+#define PI 3.1416
+float area (float a)
+		 	{
+	 		float res1;
+	 		res1=4*PI*pow(a,2);
+	 		return res1;  //devolvemos nuestro resultado
+	 	
+		 }
+float volumen (float b)
+	 {
+	 	float res1;
+	 	res1=pow(b,3)*PI*4/3;
+	 	return res1;
+	 }
