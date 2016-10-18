6a00bf98230759e57d29ec6542609e9d1a3cee34
shadexlol <shadexn1@gmail.com>
2016-10-18 23:56:50 +0200

    Add files via upload

---
+++ CARPETA-PRACTICA-2/p2ej8.c
@@ -0,0 +1,19 @@
+#include <math.h>
+
+int main()
+{
+	float x;
+	float mi_resultado;
+	float tu_resultado;
+	
+	// Da un valor a x del cual quieras saber su raiz cuadrada
+	x =
+	
+	// Completa esta línea usando pow() para calcular la raiz cuadrada de x
+	tu_resultado =
+	
+	// Yo calculo ahora la raiz cuadrada para que compares con tu resultado
+	mi_resultado = sqrt(x);
+
+	return 0;
+}
