0c25de49ba59167358f570ef0e0060f8d3809c3a
shadexlol <shadexn1@gmail.com>
2016-10-18 23:58:28 +0200

    Add files via upload

---
+++ p3ej4.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/* Prototipo de la funcion "calificar" */
+float calificar (float, float, float, float, float, float);
+
+int main (void)
+{
+	/* Define a partir de aqui las variables en las que guardaras cada 
+	   una de las calificaciones. OJO! Tienen decimales! */
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	/* Empezamos el codigo. Primero: pedir por teclado, una por una, todas las calificaciones
+	   de la evaluacion continua */
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   	
+	/* Segundo: Haz los calculos necesarios para hallar la nota de teoria
+	   y de programacion */
+
+
+
+
+
+
+
+
+	/* Tercero: dependiendo de cuáles hayan sido los resultados, imprime
+	   en pantalla la nota final, o el mensaje NO APTO, según corresponda */
+
+
+
+	   
+	
+	return 0;	
+}
+
+/* Cuerpo de la funcion "calificar" */
+float calificar (float, float, float, float, float, float)
+{
+	/* Pon aquí las variables que necesites en esta función */
+	
+	
+	
+	
+	/* Y a partir de aquí, el código */
+	
+	
+	
+}
+
