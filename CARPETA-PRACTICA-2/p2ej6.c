05e151cb28d25cb2d1cbb973d61f1d061543d4b9
shadexlol <shadexn1@gmail.com>
2016-10-19 08:14:10 +0200

    Update p2ej6.c

--- CARPETA-PRACTICA-2/p2ej6.c
+++ CARPETA-PRACTICA-2/p2ej6.c
@@ -1,18 +1,14 @@
 int incrementa (int);
 int main()
 {
-	int a,res1,res2,res3;
+	int a;
 	a = 3;
 	
-	res1 = incrementa(a);
-	res2 = incrementa(incrementa(a));
-	res3 = incrementa(incrementa(incrementa(a)));
+	a = incrementa(a);
+	a = incrementa(a);
+	a = incrementa(a);
 	
-	printf
-	("Si incrementamos una vez = %d\n Si incrementamos 2 veces =  %d \n Si incrementamos  3 veces = %d \n Esto ocurre ya que cada vez que incrementamos, nuestra funcion suma un 1 \n",res1, res2, res3);
-	
- 
-	// cu·nto vale "a" al final	
+	// cu√°nto vale "a" al final	
 	
 	 return 0;
 }
