fdc7387aee63dc1b2abc4dd468bad6f011363a06
shadexlol <shadexn1@gmail.com>
2016-10-26 10:17:09 +0200

    Create p3ejc1.c

---
+++ PRACTICA-3/p3ejc1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+	
+	int a,b,c;
+	
+	printf("Dame un año y te digo si es bisiesto o que\n");
+	scanf("%d",&a);
+	
+	c = a%4;
+	
+	if (c==0){
+		printf("El año es bisiesto cabesa\n");
+	
+	}
+	
+	else {
+		printf("XDDDD LOMAAAAAAAAA ESTE NO ES BISIESTO XDDDDDDDDDDDDDD\n");
+	}
+		
+	
+	
+	return 0;
+
+}
