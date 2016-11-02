d5153dc9e6406a6ea5ec496d71f894ca267e652e
shadexlol <shadexn1@gmail.com>
2016-11-02 11:43:09 +0100

    Create p4ej3.c

---
+++ PRACTICA-4/p4ej3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+	
+	int a,b,c,n;
+	printf("dame un numero que hago una cuenta de 1 hasta tu numero");
+	scanf("%d",&n);
+	c = 1;
+	for (a=1; a<=n ; a++){
+		c=(c*a);
+		}
+	printf ("%d",c);
+		
+	
+return 0;
+}
