a42eccb2427bcbaf91aa69e098beb907554e5cf3
shadexlol <shadexn1@gmail.com>
2016-11-08 16:57:53 +0100

    Update p4ej7.c

--- PRACTICA-4/p4ej7.c
+++ PRACTICA-4/p4ej7.c
@@ -2,14 +2,20 @@
 
 int main()
 {
-	int a,b;
-	
-	for (a=0;a<10;a++)
-	{
-		for (b=0;b<10;b++)
-		{
-			printf ("%d%d\n", a, b);
+	int a,b,c,d;
+		for (a=0;a<2;a++){
+		
+			for (b=0;b<2;b++){
+		
+				for (c=0;c<2;c++){
+						
+						for (d=0;d<2;d++) {
+							
+							printf("%d%d%d%d \n",a,b,c,d);
+						}
 		}
+		
+			}
 	}	
 	return 0;
 }
