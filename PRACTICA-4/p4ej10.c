dd838f9b90b1919a01413fb18424c053de5a0d2b
shadexlol <shadexn1@gmail.com>
2016-11-09 16:26:28 +0100

    Update p4ej10.c

--- PRACTICA-4/p4ej10.c
+++ PRACTICA-4/p4ej10.c
@@ -24,3 +24,30 @@ int main () {
 	return 0;
 }
  		
+// OTRA FORMA DE HACERLO ESTA FORMA ES LA QUE YO HE PENSADO DESPUES DE HACER ESTE CODIGO DE AQUI ARRIBA, LO MIRE Y LO PENSE Y LUEGO LO ELABORE
+// DE LA FORMA INDICADA.
+
+
+
+#include <stdio.h>
+#include <conio.h>
+
+int main (void) {
+
+
+	int primero, segundo, sucesion, c;
+	
+	primero=0;
+	segundo=1;
+		
+		for (c=0; c<10; c++) {
+			if (c<=1)
+				sucesion = c;
+			else {
+				sucesion = primero + segundo;
+				primero = segundo;
+				segundo = sucesion;
+			}
+		printf ("%d\n",sucesion);
+		}
+				
